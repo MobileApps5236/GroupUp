0efbeeaa8f0a7a737e912e8d4629583278c63321
trobb10 <amr2146@gmail.com>
2018-02-21 13:26:22 -0500

    simple onClick method implemented and a movement to the activity

--- app/src/main/java/com/example/tonyrobb/groupup/Login.java
+++ app/src/main/java/com/example/tonyrobb/groupup/Login.java
@@ -1,7 +1,10 @@
 package com.example.tonyrobb.groupup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button loginBtn = findViewById(R.id.btnLogin);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainPage.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private static void loginOperation(){
+
     }
 }
