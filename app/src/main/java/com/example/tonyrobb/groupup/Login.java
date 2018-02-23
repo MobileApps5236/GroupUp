7a90564bd519bd4c6f89562a18d76343900e4fa5
Tanzim Khan <khan.498@osu.edu>
2018-02-23 18:15:44 -0500

    Checkpoint 3 stuff

--- app/src/main/java/com/example/tonyrobb/groupup/Login.java
+++ app/src/main/java/com/example/tonyrobb/groupup/Login.java
@@ -3,8 +3,10 @@ package com.example.tonyrobb.groupup;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
 
@@ -14,17 +16,32 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         Button loginBtn = findViewById(R.id.btnLogin);
+        final EditText username = findViewById(R.id.username);
+        final EditText password = findViewById(R.id.password);
+        Log.v("TAG", "onCreate triggered");
         loginBtn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainPage.class);
-                startActivity(intent);
+                loginOperation(username.getText().toString(), password.getText().toString());
             }
         });
     }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v("TAG", "onPause triggered");
+    }
 
-    private static void loginOperation(){
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.v("TAG","onResume triggered");
+    }
 
+    private void loginOperation(String username, String password){
+        Intent intent = new Intent(getApplicationContext(), MainPage.class);
+        intent.putExtra("username", username);
+        startActivity(intent);
     }
 }
