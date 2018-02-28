4fbb5e85b9734b4855f6e198c3e354f1bd40b1f5
pinsky22 <pinsky.22@osu.edu>
2018-02-28 18:40:50 -0500

    Finished basic log in and sign up with Firebase

---
+++ app/src/main/java/com/example/tonyrobb/groupup/CreateAccount.java
@@ -0,0 +1,105 @@
+package com.example.tonyrobb.groupup;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class CreateAccount extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        auth = FirebaseAuth.getInstance();
+
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //fab.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View view) {
+        //        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        //                .setAction("Action", null).show();
+        //    }
+        //});
+
+        Button btnSignIn = (Button) findViewById(R.id.sign_in_button);
+        Button btnSignUp = (Button) findViewById(R.id.sign_up_button);
+        final EditText inputEmail = (EditText) findViewById(R.id.email);
+        final EditText inputPassword = (EditText) findViewById(R.id.password);
+        //final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        Button btnResetPassword = (Button) findViewById(R.id.btn_reset_password);
+
+        btnResetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CreateAccount.this, ResetPassword.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        btnSignUp.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = inputEmail.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Please enter an email address", Toast.LENGTH_SHORT).show();
+                }
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Please enter a password", Toast.LENGTH_SHORT).show();
+                }
+
+                //progressBar.setVisibility(View.VISIBLE);
+
+                //This is where a user is created
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(CreateAccount.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Toast.makeText(CreateAccount.this, "Created user: " + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                        //progressBar.setVisibility(View.GONE);
+
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(CreateAccount.this, "Authentication failed: " + task.getException(), Toast.LENGTH_SHORT).show();
+                        } else {
+                            Intent intent = new Intent(CreateAccount.this, MainPage.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                });
+            }
+        }));
+
+    }
+
+}
--- app/src/main/java/com/example/tonyrobb/groupup/Login.java
+++ app/src/main/java/com/example/tonyrobb/groupup/Login.java
@@ -1,28 +1,103 @@
 package com.example.tonyrobb.groupup;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 public class Login extends AppCompatActivity {
+
+    private EditText inputEmail, inputPassword;
+    private Button btnResetPassword;
+    private ProgressBar progressBar;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        auth = FirebaseAuth.getInstance();
+
+        //if (auth.getCurrentUser() != null) {
+        //    Intent intent = new Intent(Login.this, MainPage.class);
+        //    startActivity(intent);
+        //}
+
+        // If user is not logged in then continue as normal
         setContentView(R.layout.activity_login);
 
-        Button loginBtn = findViewById(R.id.btnLogin);
+        Button loginBtn = (Button) findViewById(R.id.btnLogin);
+        Button btnSignUp = (Button) findViewById(R.id.btnCreateAccount);
+        Button btnReset = (Button) findViewById(R.id.btn_reset_password);
+
+        inputEmail = (EditText) findViewById(R.id.username);
+        inputPassword = (EditText) findViewById(R.id.password);
+
+        // Get another instance
+        auth = FirebaseAuth.getInstance();
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick (View view) {
+                Intent intent = new Intent(getApplicationContext(), CreateAccount.class);
+                startActivity(intent);
+            }
+        });
+
         loginBtn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainPage.class);
-                startActivity(intent);
+                String email = inputEmail.getText().toString();
+                final String password = inputPassword.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Time to authenticate user
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (!task.isSuccessful()) {
+                                    if (password.length() > 6) {
+                                        Toast.makeText(Login.this, "Wrong email or password", Toast.LENGTH_LONG).show();
+                                    } else {
+                                        Toast.makeText(Login.this, "Password too short (under 6 characters)", Toast.LENGTH_LONG).show();
+                                    }
+                                } else {
+                                    Intent intent = new Intent(Login.this, MainPage.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }
+                        });
             }
         });
     }
+
+
 
     private static void loginOperation(){
 
