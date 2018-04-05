aa6fd0689d720889e2f6d6069bca5c758aee13c4
Tanzim Khan <khan.498@osu.edu>
2018-04-04 20:16:26 -0400

    Added thet other thing (scaled down profile pic after retriving)

--- app/src/main/java/com/example/tonyrobb/groupup/DownloadImageTask.java
+++ app/src/main/java/com/example/tonyrobb/groupup/DownloadImageTask.java
@@ -2,6 +2,7 @@ package com.example.tonyrobb.groupup;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.ImageView;
@@ -9,26 +10,47 @@ import android.widget.ImageView;
 import java.io.InputStream;
 
 public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-    ImageView bmImage;
+    ImageView imageViewProfile;
 
     public DownloadImageTask(ImageView bmImage) {
-        this.bmImage = bmImage;
+        this.imageViewProfile = bmImage;
     }
 
     protected Bitmap doInBackground(String... urls) {
         String urldisplay = urls[0];
-        Bitmap mIcon11 = null;
+        Bitmap scaledImageBitmap = null;
+
+        int scaledWidth = imageViewProfile.getDrawable().getIntrinsicWidth() + 50;
+        int scaledHeight = imageViewProfile.getDrawable().getIntrinsicHeight() + 50;
+
         try {
             InputStream in = new java.net.URL(urldisplay).openStream();
-            mIcon11 = BitmapFactory.decodeStream(in);
+            scaledImageBitmap = getResizedBitmap(BitmapFactory.decodeStream(in), scaledWidth, scaledHeight);
         } catch (Exception e) {
             Log.e("Error", e.getMessage());
             e.printStackTrace();
+            scaledImageBitmap = null;
         }
-        return mIcon11;
+        return scaledImageBitmap;
     }
 
     protected void onPostExecute(Bitmap result) {
-        bmImage.setImageBitmap(result);
+        imageViewProfile.setImageBitmap(result);
+    }
+
+    private Bitmap getResizedBitmap(Bitmap bm, int newWidth, int newHeight) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // CREATE A MATRIX FOR THE MANIPULATION
+        Matrix matrix = new Matrix();
+        // RESIZE THE BIT MAP
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        // "RECREATE" THE NEW BITMAP
+        Bitmap resizedBitmap = Bitmap.createBitmap(
+                bm, 0, 0, width, height, matrix, false);
+        return resizedBitmap;
     }
 }
